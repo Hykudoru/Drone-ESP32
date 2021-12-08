68a4278b2eb489cfbd35354841113dda4a78481b
Alex Fish <iamalexfish@gmail.com>
2021-12-08 16:39:05 -0600

    oled

--- src/main.cpp
+++ src/main.cpp
@@ -4,77 +4,69 @@
 #include <WiFi.h>
 //#include <WiFiClient.h>
 //#include <WiFiScan.h>
-//#include <Adafruit_GFX.h>
-//#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+const int I2C_ADDR = 0x3C; // Can be shared with other I2C devices
+const int BUTTON_A = 15;//GPIO 15 or A8
+const int BUTTON_B = 32;
+const int BUTTON_C = 14;
 
 #if defined(ESP32)
   const int BAUD_RATE = 115200;
-  #define BUTTON_A = 15;//GPIO 15 or A8
-  #define BUTTON_B = 32;
-  #define BUTTON_C = 14;
 #else
   const int BAUD_RATE = 9600;
 #endif
 
-const int I2C_ADDR = 0x3C; // Can be shared with other I2C devices
-int t = 0;
-
-//wifi client
-#define wifi_mode = WIFI_STA;
-
-void wifiSetup() {
-  char ssid[] = "";//ssid
-  char password[] = "";
-  wl_status_t status = WiFi.status();
-  static String macAddr = WiFi.macAddress();
-  int ip = WiFi.broadcastIP();
-  int localIP = WiFi.localIP();
-  
-}
-
-void broadcastWifi()
-{
-
-}
-
-void scanWifi() {
-
-  
-}
-
-int test() {
-
-}
-
-int test2();// {}
-
 //pfunc can be reasigned at runtime to change the desired procedure invoked inside the default loop function.
-typedef void (*pointerFunction)(void);//{&test};
+typedef void (*pointerFunction)(void);
 pointerFunction pfunc = &loop;
 //typedef void (*loop_func)();
 
+int t = 0;
+Adafruit_SSD1306 display = Adafruit_SSD1306(128, 32, &Wire);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(BAUD_RATE);
   Serial.println("");
 
-  Serial.println(WiFi.status());
-  Serial.println(WiFi.macAddress());
-  Serial.println(WiFi.localIP());
-  Serial.println(WiFi.localIPv6());
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.display();
+  display.clearDisplay();
+ 
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.println("Setup...");
+  display.display();
+  delay(500);
+
+  pinMode(BUTTON_A, INPUT_PULLUP);
+  pinMode(BUTTON_B, INPUT_PULLUP);
+  pinMode(BUTTON_C, INPUT_PULLUP);
 
   WiFi.mode(WIFI_STA);//Stationary mode
-  WiFi.disconnect();//Disconnect incase was connected to AP 
+  WiFi.disconnect();//Disconnect incase was connected to AP  
 }
 
 
 void loop() {
   // put your main code here, to run repeatedly:
   Serial.print("Time elapsed: ");
-  Serial.println(t++);
+  Serial.println(t);
+  
+  display.clearDisplay();
+  display.setCursor(0,32/2);
+  display.print("Time elapsed: ");
+  display.print(t);
+  display.print("s");
+  yield();
+  display.display();
   delay(1000);
+  t++;
 
+  /*
   int n = WiFi.scanNetworks();
   
   if (n == 0) {
@@ -105,6 +97,7 @@ void loop() {
 
   //default delay rescan
   delay(5000);
+  */
 }
 
 //pLoop* = function();
