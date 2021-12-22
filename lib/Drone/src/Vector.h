e951231be980dd26f12e09ea6e4de6a0db81fb52
Alex Fish <iamalexfish@gmail.com>
2021-12-22 13:18:11 -0600

    updated Vector3 lib

--- lib/Drone/src/Vector.h
+++ lib/Drone/src/Vector.h
@@ -10,5 +10,33 @@ struct Vector3
     Vector3();
     Vector3(float xVal, float yVal, float zVal);
     Vector3(float xyz[]);
+    
+    Vector3 operator+(const Vector3& vec)
+    {
+        Vector3 vectorSum;
+        vectorSum.x = this->x + vec.x;
+        vectorSum.y = this->y + vec.y;
+        vectorSum.z = this->z + vec.z;
+        return vectorSum;
+    }
+
+    Vector3 operator-(const Vector3& vec)
+    {
+        Vector3 vectorDiff;
+        vectorDiff.x = this->x - vec.x;
+        vectorDiff.y = this->y - vec.y;
+        vectorDiff.z = this->z - vec.z;
+        return vectorDiff;
+    }
+    /*
+    Vector3 operator+=(const Vector3& vec)
+    {
+        this->x += vec.x;
+        this->y += vec.y;
+        this->z += vec.z;
+
+        return this;
+    }
+    */
 };
 #endif
