77176f6ba0a11678a8955bb786d91cb3a7e8c667
Alex Fish <iamalexfish@gmail.com>
2022-02-09 16:57:39 -0600

    incomp

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <SparkFun_Qwiic_Joystick_Arduino_Library.h>
+#include <SparkFun_I2C_Mux_Arduino_Library.h>
 
 const int I2C_ADDR = 0x3C; // Can be shared with other I2C devices
 const int BUTTON_A = 15;//GPIO 15 or A8
@@ -24,8 +25,12 @@ typedef void (*pointerFunction)(void);
 pointerFunction pfunc;
 Adafruit_SSD1306 display = Adafruit_SSD1306(128, 32, &Wire);
 
-JOYSTICK joystick_1;
-JOYSTICK joystick_2;
+QWIICMUX mux;
+JOYSTICK joystick;
+int joystickZeroOffsetX = 0;
+int joystickZeroOffsetY = 0;
+// JOYSTICK joystick_1;
+// JOYSTICK joystick_2;
 
 //timer
 void mode_1() {
@@ -104,6 +109,35 @@ void mode_3(){
   display.display();
 }
 
+void calibrateJoystick(int muxPort)
+{
+    mux.enablePort(muxPort);
+    Serial.println(mux.getPort());
+    joystickZeroOffsetX = joystick.getHorizontal();
+    joystickZeroOffsetY = joystick.getVertical();
+    mux.disablePort(muxPort);
+}
+void muxJoystick(int muxPort) {
+  
+  mux.enablePort(muxPort);
+  Serial.println(mux.getPort());
+
+  int x = joystick.getHorizontal() - joystickZeroOffsetX;
+  int y = joystick.getVertical() - joystickZeroOffsetY;
+
+  // fix dir of x axis
+  if (x != 514)
+  {
+    x = 1023 - x;
+  }
+
+  Serial.println(String("Joystick ")+muxPort+" X: "+x);
+  Serial.println(String("Joystick ")+muxPort+" Y: "+y);
+  Serial.println(String("Joystick ")+muxPort+" Button: "+joystick.getButton());
+
+  mux.disablePort(muxPort);
+}
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(BAUD_RATE);
@@ -120,21 +154,26 @@ void setup() {
   display.display();
   delay(500);
 
-  pinMode(BUTTON_A, INPUT_PULLUP);
-  pinMode(BUTTON_B, INPUT_PULLUP);
-  pinMode(BUTTON_C, INPUT_PULLUP);
-
-  WiFi.mode(WIFI_STA);//Stationary mode
-  WiFi.disconnect();//Disconnect incase was connected to AP
-
   pfunc = &mode_1;  
 
-  while(!joystick_1.begin())
-  {
-    Serial.println("Joystick not detected.");
-    delay(1000);
-  }
-}
+  // while(!joystick.begin())
+  // {
+  //   Serial.println("Joystick not detected.");
+  //   delay(1000);
+  // }
+
+  joystick.begin();
+  Wire.begin();
+  mux.begin();
+  muxJoystick(1); // force read potentially bad init readings
+  muxJoystick(2); // force read potentially bad init readings
+
+  //calibrateJoystick(1); // calculates joystick zero offset
+  //calibrateJoystick(2); // calculates joystick zero offset
+
+  delay(100);
+ }
+
 
 
 void loop() {
@@ -143,25 +182,10 @@ void loop() {
   if (digitalRead(BUTTON_B) == 0) pfunc = &mode_2;
   if (digitalRead(BUTTON_C) == 0) pfunc = &mode_3;
   
-  (*pfunc)();
+  //(*pfunc)();
 
-  
-  uint16_t x = joystick_1.getHorizontal();
-  uint16_t y = joystick_1.getVertical();
+  muxJoystick(1);
+  muxJoystick(2);
 
-  // fix dir of x axis
-  if (x != 514)
-  {
-    x = 1023 - x;
-  }
-
-  Serial.println(String("Joystick_1 X: ")+x);
-  Serial.println(String("Joystick_1 Y: ")+y);
-  Serial.println(String("Joystick_1 button: ")+joystick_1.getButton());
-  
-  /*
-  
-  */
+  delay(200);
 }
-
-//pLoop* = function();
