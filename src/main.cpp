d5dc15ee01893d6c367ab2f13968ef7bbd50dbac
Hykudoru <iamalexfish@gmail.com>
2022-02-27 18:02:03 -0600

    Mouse.move(mouseX, -mouseY, scroll->y);

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 #include <Adafruit_SSD1306.h>
 #include <Vector.h>
 #include <MuxJoystick.h>
+#include <Mouse.h>
+#include <Keyboard.h>
 
 #if defined(ESP32)
   const int BAUD_RATE = 115200;
@@ -17,6 +19,16 @@
   #define BUTTON_A 9
   #define BUTTON_B 6
   #define BUTTON_C 5
+  #define BUTTON_D 12C 
+  #define BUTTON_MOUSE_LEFT 0xA0
+  #define BUTTON_MOUSE_RIGHT 0xA1
+  #define BUTTON_SWAP_JOYSTICKS 0xA2
+  
+  #define ANALOG_PIN_3 0xA3
+  #define ANALOG_PIN_4 0xA4
+  #define ANALOG_PIN_5 0xA5
+
+  #define LED_GREEN 8
 #endif
 
 //pfunc can be reasigned at runtime to change the desired procedure invoked inside the default loop function.
@@ -53,6 +65,15 @@ MuxJoystick rightJoystick(RIGHT_JOYSTICK_MUX_PORT);
 //     Serial.println("Calibrating...");
 //   }
 
+#define LSTICK_LEFT_LED 10
+#define LSTICK_RIGHT_LED 11
+#define LSTICK_DOWN_LED 12
+#define LSTICK_UP_LED 13
+
+#define RSTICK_LEFT_LED 9//10
+#define RSTICK_RIGHT_LED 9//11
+#define RSTICK_DOWN_LED 9//12
+#define RSTICK_UP_LED 9//13
 
 void setup() 
 {
@@ -72,37 +93,160 @@ void setup()
   leftJoystick.Start();
   rightJoystick.Start();
 
+  Mouse.begin();
+  Keyboard.begin();
+
   oled.display();
 
   pinMode(BUTTON_A, INPUT_PULLUP);
   pinMode(BUTTON_B, INPUT_PULLUP);
   pinMode(BUTTON_C, INPUT_PULLUP);
+
+  pinMode(LED_GREEN, OUTPUT);
+
+  pinMode(LSTICK_LEFT_LED, OUTPUT);
+  pinMode(LSTICK_RIGHT_LED, OUTPUT);
+  pinMode(LSTICK_DOWN_LED, OUTPUT);
+  pinMode(LSTICK_UP_LED, OUTPUT);
+  pinMode(RSTICK_LEFT_LED, OUTPUT);
+  pinMode(RSTICK_RIGHT_LED, OUTPUT);
+  pinMode(RSTICK_DOWN_LED, OUTPUT);
+  pinMode(RSTICK_UP_LED, OUTPUT);
   // ptrMode = &mode_1;  
 
+  digitalWrite(LED_GREEN, HIGH);
   delay(1000);
+  digitalWrite(LED_GREEN, LOW);
  }
 
-bool Send()
+    
+void mouseLoop()
 {
-  
-  return 0;
+  static MuxJoystick *mouse = &leftJoystick;
+  static MuxJoystick *scroll = &rightJoystick;
+  static int maxSpeed = 40;
+  static bool mouseActive = true;
+    
+  if (Serial.read() == 'p' || digitalRead(BUTTON_A) == HIGH)
+  {
+    mouseActive = !mouseActive;
+  }
+
+  if (mouseActive) 
+  { 
+    if (mouse->x != 0 || mouse->y != 0)
+    {
+      int dirX = mouse->x/abs(mouse->x);// -1 or 1
+      int dirY = mouse->y/abs(mouse->y);// -1 or 1
+      int mouseX = dirX * map(abs(mouse->x), 0, 100, 0, maxSpeed);
+      int mouseY = dirY * map(abs(mouse->y), 0, 100, 0, maxSpeed);
+      
+      Mouse.move(mouseX, -mouseY, scroll->y);
+
+      Serial.println("-----------------------");
+      Serial.print(String("Mouse \t x:")+mouse->x+", y:"+ mouse->y);
+      Serial.println(String("\t dirX:")+dirX+" dirY:"+dirY);
+      Serial.println("-----------------------");
+      
+      
+    }
+    if (mouse->button)
+    {
+      Keyboard.press(KEY_LEFT_CTRL);
+      Keyboard.press('c');
+      Keyboard.releaseAll();
+    }
+    if (scroll->button) {
+      Keyboard.press(KEY_LEFT_CTRL);
+      Keyboard.press('v');
+      Keyboard.releaseAll();
+    }
+      //Keyboard.write(KEY_RETURN);
+
+    delay(5);
+  }
+    // if (Keyboard.)
 }
 
-//char *mode = "mode_1";
+void Send()
+{
+}
 
 void loop() 
 {
   oled.clearDisplay();
-  // if (digitalRead(BUTTON_A) == 0) mode = "mode_1";
-  // if (digitalRead(BUTTON_B) == 0) mode = "mode_2";
-  // if (digitalRead(BUTTON_C) == 0) mode = "mode_3";
+  oled.setCursor(0, 0);
+  // if (digitalRead(BUTTON_A) == 0) ptrMode = mode_1;
+  // if (digitalRead(BUTTON_B) == 0) ptrMode = mode_2;
+  // if (digitalRead(BUTTON_C) == 0) ptrMode = mode_3;
   
-  Vector3<int> l = leftJoystick.Read();
-  Vector3<int> r = rightJoystick.Read();
+  leftJoystick.Read();
+  rightJoystick.Read();
+  mouseLoop();
+  // LEFT JOYSTICK LEDS
+
+  // // LEFT
+  // if (leftJoystick.x < 0) {
+  //   analogWrite(LSTICK_LEFT_LED, map(abs(leftJoystick.x), 0, 100, 0, 255));
+  //   digitalWrite(LSTICK_RIGHT_LED, LOW);
+  // } // RIGHT
+  // else if(leftJoystick.x > 0) {
+  //   analogWrite(LSTICK_RIGHT_LED, map(abs(leftJoystick.x), 0, 100, 0, 255));
+  //   digitalWrite(LSTICK_LEFT_LED, LOW);
+  // } // ZERO
+  // else {
+  //    digitalWrite(LSTICK_LEFT_LED, LOW);
+  //   digitalWrite(LSTICK_RIGHT_LED, LOW);
+  // }
+  // // DOWN
+  // if (leftJoystick.y < 0) {
+  //   analogWrite(LSTICK_DOWN_LED, map(abs(leftJoystick.y), 0, 100, 0, 255));
+  //   digitalWrite(LSTICK_UP_LED, LOW);
+  // }// UP
+  // else if(leftJoystick.y > 0) {
+  //   analogWrite(LSTICK_UP_LED, map(abs(leftJoystick.y), 0, 100, 0, 255));
+  //   digitalWrite(LSTICK_DOWN_LED, LOW);
+  // } // ZERO
+  // else {
+  //   digitalWrite(LSTICK_DOWN_LED, LOW);
+  //   digitalWrite(LSTICK_UP_LED, LOW);
+  // }
+
+  // // RIGHT JOYSTICK LEDS
+  
+  // // LEFT
+  // if (rightJoystick.x < 0) {
+  //   analogWrite(RSTICK_LEFT_LED, map(abs(rightJoystick.x), 0, 100, 0, 255));
+  //   digitalWrite(RSTICK_RIGHT_LED, LOW);
+  // } // RIGHT
+  // else if(rightJoystick.x > 0) {
+  //   analogWrite(RSTICK_RIGHT_LED, map(abs(rightJoystick.x), 0, 100, 0, 255));
+  //   digitalWrite(RSTICK_LEFT_LED, LOW);
+  // }// ZERO
+  // else {
+  //   digitalWrite(RSTICK_LEFT_LED, LOW);
+  //   digitalWrite(RSTICK_RIGHT_LED, LOW);
+  // } 
+
+  //   // DOWN
+  // if (rightJoystick.y < 0) {
+  //   analogWrite(RSTICK_DOWN_LED, map(abs(rightJoystick.y), 0, 100, 0, 255));
+  //   digitalWrite(RSTICK_UP_LED, LOW);
+  // }// UP
+  // else if(rightJoystick.y > 0) {
+  //   analogWrite(RSTICK_UP_LED, map(abs(rightJoystick.y), 0, 100, 0, 255));
+  //   digitalWrite(RSTICK_DOWN_LED, LOW);
+  // } // ZERO
+  // else {
+  //   digitalWrite(RSTICK_DOWN_LED, LOW);
+  //   digitalWrite(RSTICK_UP_LED, LOW);
+  // }
+  
   Send();
 
+  
   //(*ptrMode)();
   
   oled.display();
-  delay(20);
+  //delay(2);
 }
